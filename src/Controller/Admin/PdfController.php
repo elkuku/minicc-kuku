f63473e66d67c68797d8f12ddd96fae04e437bc3
Nikolai Plath <der.el.kuku@gmail.com>
2018-04-05 14:19:54 -0500

    Add mail planillas

--- src/Controller/Admin/PdfController.php
+++ src/Controller/Admin/PdfController.php
@@ -15,68 +15,6 @@ use Symfony\Component\HttpFoundation\Request;
  */
 class PdfController extends Controller
 {
-    /**
-     * @Route("/planillas", name="planillas")
-     * @Security("has_role('ROLE_ADMIN')")
-     *
-     * @return PdfResponse
-     */
-    public function planillasAction()
-    {
-        $year  = date('Y');
-        $month = date('m');
-
-        $stores = $this->getDoctrine()
-            ->getRepository(Store::class)
-            ->findAll();
-
-        $factDate = $year.'-'.$month.'-1';
-
-        if (1 == $month) {
-            $prevYear  = $year - 1;
-            $prevMonth = 12;
-        } else {
-            $prevYear  = $year;
-            $prevMonth = $month - 1;
-        }
-
-        $prevDate = $prevYear.'-'.$prevMonth.'-01';
-
-        $repo = $this->getDoctrine()
-            ->getRepository(Transaction::class);
-
-        $storeData = [];
-
-        /* @type Store $store */
-        foreach ($stores as $store) {
-            $storeData[$store->getId()]['saldoIni']     = $repo->getSaldoALaFecha(
-                $store,
-                $prevYear.'-'.$prevMonth.'-01'
-            );
-            $storeData[$store->getId()]['transactions'] = $repo->findMonthPayments(
-                $store,
-                $prevMonth,
-                $prevYear
-            );
-        }
-
-        $html = $this->renderView(
-            'admin/planillas-pdf.html.twig',
-            [
-                'factDate'  => $factDate,
-                'prevDate'  => $prevDate,
-                'stores'    => $stores,
-                'storeData' => $storeData,
-            ]
-        );
-
-        $filename = sprintf('planillas-%d-%d.pdf', $year, $month);
-
-	    Return new PdfResponse(
-	    	$this->get('knp_snappy.pdf')->getOutputFromHtml($html),
-		    $filename
-	    );
-    }
 	/**
 	 * @Route("/store-transaction-pdf/{id}/{year}", name="store-transaction-pdf")
 	 * @Security("has_role('ROLE_ADMIN')")
---
+++ src/Controller/Admin/PlanillasController.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Store;
+use App\Entity\Transaction;
+use Knp\Bundle\SnappyBundle\Snappy\Response\PdfResponse;
+use Knp\Snappy\Pdf;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Class PlanillasController
+ */
+class PlanillasController extends Controller
+{
+	/**
+	 * @Route("/planillas-mail", name="planillas-mail")
+	 * @Security("has_role('ROLE_ADMIN')")
+	 *
+	 * @return Response
+	 */
+	public function mailAction()
+	{
+		$year  = date('Y');
+		$month = date('m');
+
+		$fileName = "planillas-$year-$month.pdf";
+		$html     = "Attachment: " . $fileName;
+
+		$pdf = $this->get('knp_snappy.pdf')
+			->getOutputFromHtml(
+				$this->getPlanillasHtml($year, $month)
+			);
+
+		$message = (new \Swift_Message())
+			->setSubject("Planillas $year-$month")
+			->setFrom('minicckuku@gmail.com')
+			->setTo('minicckuku@gmail.com')
+			->setBody($html)
+			->attach(new \Swift_Attachment($pdf, $fileName, 'application/pdf'));
+
+		$count = $this->get('mailer')->send($message);
+
+		if (!$count)
+		{
+			$this->addFlash('danger', 'There was an error sending mail...');
+		}
+		else
+		{
+			$this->addFlash('success', ($count > 1 ? $count . ' mails have been sent.' : 'One mail has been sent.'));
+		}
+
+		return $this->render('admin/tasks.html.twig');
+	}
+
+	/**
+	 * @Route("/planillas", name="planillas")
+	 * @Security("has_role('ROLE_ADMIN')")
+	 *
+	 * @return PdfResponse
+	 */
+	public function downloadAction()
+	{
+
+		$year  = date('Y');
+		$month = date('m');
+
+		$filename = sprintf('planillas-%d-%d.pdf', $year, $month);
+		$html     = $this->getPlanillasHtml($year, $month);
+
+		Return new PdfResponse(
+			$this->get('knp_snappy.pdf')->getOutputFromHtml($html),
+			$filename
+		);
+	}
+
+	/**
+	 * @param $year
+	 * @param $month
+	 *
+	 * @return string
+	 */
+	private function getPlanillasHtml($year, $month)
+	{
+		$stores = $this->getDoctrine()
+			->getRepository(Store::class)
+			->findAll();
+
+		$factDate = $year . '-' . $month . '-1';
+
+		if (1 == $month)
+		{
+			$prevYear  = $year - 1;
+			$prevMonth = 12;
+		}
+		else
+		{
+			$prevYear  = $year;
+			$prevMonth = $month - 1;
+		}
+
+		$prevDate = $prevYear . '-' . $prevMonth . '-01';
+
+		$repo = $this->getDoctrine()
+			->getRepository(Transaction::class);
+
+		$storeData = [];
+
+		/* @type Store $store */
+		foreach ($stores as $store)
+		{
+			$storeData[$store->getId()]['saldoIni']     = $repo->getSaldoALaFecha(
+				$store,
+				$prevYear . '-' . $prevMonth . '-01'
+			);
+			$storeData[$store->getId()]['transactions'] = $repo->findMonthPayments(
+				$store,
+				$prevMonth,
+				$prevYear
+			);
+		}
+
+		return $this->renderView(
+			'admin/planillas-pdf.html.twig',
+			[
+				'factDate'  => $factDate,
+				'prevDate'  => $prevDate,
+				'stores'    => $stores,
+				'storeData' => $storeData,
+			]
+		);
+	}
+}
