eeffc63a9e59cbc4be26bec646b14d387dbe90de
Nikolai Plath <der.el.kuku@gmail.com>
2018-06-18 11:02:09 -0500

    Review Entities

--- src/Entity/Deposit.php
+++ src/Entity/Deposit.php
@@ -13,176 +13,172 @@ use Doctrine\ORM\Mapping\ManyToOne;
 use JsonSerializable;
 
 /**
- * Deposit
- *
- * @ORM\Table(name="deposit")
  * @ORM\Entity(repositoryClass="App\Repository\DepositRepository")
  */
 class Deposit implements JsonSerializable
 {
-	/**
-	 * @var integer
-	 *
-	 * @ORM\Column(name="id", type="integer")
-	 * @ORM\Id
-	 * @ORM\GeneratedValue(strategy="IDENTITY")
-	 */
-	private $id;
+    /**
+     * @var integer
+     *
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
 
-	/**
-	 * @var PaymentMethod
-	 * @ManyToOne(targetEntity="PaymentMethod")
-	 */
-	private $entity;
+    /**
+     * @var PaymentMethod
+     * @ManyToOne(targetEntity="PaymentMethod")
+     */
+    private $entity;
 
-	/**
-	 * @var \DateTime
-	 *
-	 * @ORM\Column(name="date", type="date", nullable=false)
-	 */
-	private $date;
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="date", nullable=false)
+     */
+    private $date;
 
-	/**
-	 * @var string
-	 *
-	 * @ORM\Column(type="string", length=150, nullable=false)
-	 */
-	private $document;
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=150, nullable=false)
+     */
+    private $document;
 
-	/**
-	 * @var string
-	 *
-	 * @ORM\Column(type="decimal", precision=13, scale=2, nullable=false)
-	 */
-	private $amount;
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="decimal", precision=13, scale=2, nullable=false)
+     */
+    private $amount;
 
-	/**
-	 * Get id
-	 *
-	 * @return integer
-	 */
-	public function getId()
-	{
-		return $this->id;
-	}
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
 
-	/**
-	 * Set entity
-	 *
-	 * @param PaymentMethod $entity
-	 *
-	 * @return Deposit
-	 */
-	public function setEntity(PaymentMethod $entity)
-	{
-		if (1 == $entity->getId())
-		{
-			throw new \UnexpectedValueException(
-				'The entity with ID "1" is supposed to be the BAR payment method!'
-			);
-		}
+    /**
+     * Set entity
+     *
+     * @param PaymentMethod $entity
+     *
+     * @return Deposit
+     */
+    public function setEntity(PaymentMethod $entity)
+    {
+        if (1 == $entity->getId()) {
+            throw new \UnexpectedValueException(
+                'The entity with ID "1" is supposed to be the BAR payment method!'
+            );
+        }
 
-		$this->entity = $entity;
+        $this->entity = $entity;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * Get entity
-	 *
-	 * @return PaymentMethod
-	 */
-	public function getEntity()
-	{
-		return $this->entity;
-	}
+    /**
+     * Get entity
+     *
+     * @return PaymentMethod
+     */
+    public function getEntity()
+    {
+        return $this->entity;
+    }
 
-	/**
-	 * Set date
-	 *
-	 * @param \DateTime $date
-	 *
-	 * @return Deposit
-	 */
-	public function setDate(\DateTime $date)
-	{
-		$this->date = $date;
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     *
+     * @return Deposit
+     */
+    public function setDate(\DateTime $date)
+    {
+        $this->date = $date;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * Get date
-	 *
-	 * @return \DateTime
-	 */
-	public function getDate()
-	{
-		return $this->date;
-	}
+    /**
+     * Get date
+     *
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
 
-	/**
-	 * Set document
-	 *
-	 * @param string $document
-	 *
-	 * @return Deposit
-	 */
-	public function setDocument($document)
-	{
-		$this->document = $document;
+    /**
+     * Set document
+     *
+     * @param string $document
+     *
+     * @return Deposit
+     */
+    public function setDocument($document)
+    {
+        $this->document = $document;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * Get document
-	 *
-	 * @return string
-	 */
-	public function getDocument()
-	{
-		return $this->document;
-	}
+    /**
+     * Get document
+     *
+     * @return string
+     */
+    public function getDocument()
+    {
+        return $this->document;
+    }
 
-	/**
-	 * Set amount
-	 *
-	 * @param string $amount
-	 *
-	 * @return Deposit
-	 */
-	public function setAmount($amount)
-	{
-		$this->amount = $amount;
+    /**
+     * Set amount
+     *
+     * @param string $amount
+     *
+     * @return Deposit
+     */
+    public function setAmount($amount)
+    {
+        $this->amount = $amount;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * Get amount
-	 *
-	 * @return string
-	 */
-	public function getAmount()
-	{
-		return $this->amount;
-	}
+    /**
+     * Get amount
+     *
+     * @return string
+     */
+    public function getAmount()
+    {
+        return $this->amount;
+    }
 
-	/**
-	 * Specify data which should be serialized to JSON
-	 * @link  http://php.net/manual/en/jsonserializable.jsonserialize.php
-	 * @return mixed data which can be serialized by <b>json_encode</b>,
-	 * which is a value of any type other than a resource.
-	 * @since 5.4.0
-	 */
-	public function jsonSerialize()
-	{
-		return [
-			'id'       => $this->id,
-			'amount'   => $this->amount,
-			'document' => $this->document,
-			'date'     => $this->date->format('Y-m-d'),
-		];
-	}
+    /**
+     * Specify data which should be serialized to JSON
+     * @link  http://php.net/manual/en/jsonserializable.jsonserialize.php
+     * @return mixed data which can be serialized by <b>json_encode</b>,
+     * which is a value of any type other than a resource.
+     * @since 5.4.0
+     */
+    public function jsonSerialize()
+    {
+        return [
+            'id'       => $this->id,
+            'amount'   => $this->amount,
+            'document' => $this->document,
+            'date'     => $this->date->format('Y-m-d'),
+        ];
+    }
 }
--- src/Entity/PaymentMethod.php
+++ src/Entity/PaymentMethod.php
@@ -14,57 +14,57 @@ use Doctrine\ORM\Mapping as ORM;
  * Entity
  *
  * @ORM\Table(name="payment_method")
- * @ORM\Entity
+ * @ORM\Entity(repositoryClass="App\Repository\PaymentMethodRepository")
  */
 class PaymentMethod
 {
-	/**
-	 * @var integer
-	 *
-	 * @ORM\Column(name="id", type="integer")
-	 * @ORM\Id
-	 * @ORM\GeneratedValue(strategy="IDENTITY")
-	 */
-	private $id;
+    /**
+     * @var integer
+     *
+     * @ORM\Id
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
 
-	/**
-	 * @var string
-	 *
-	 * @ORM\Column(type="string", length=150, nullable=false)
-	 */
-	private $name;
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=150, nullable=false)
+     */
+    private $name;
 
-	/**
-	 * Get id
-	 *
-	 * @return integer
-	 */
-	public function getId()
-	{
-		return $this->id;
-	}
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
 
-	/**
-	 * Set name
-	 *
-	 * @param string $name
-	 *
-	 * @return PaymentMethod
-	 */
-	public function setName($name)
-	{
-		$this->name = $name;
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return PaymentMethod
+     */
+    public function setName(string $name): self
+    {
+        $this->name = $name;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * Get name
-	 *
-	 * @return string
-	 */
-	public function getName()
-	{
-		return $this->name;
-	}
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
 }
