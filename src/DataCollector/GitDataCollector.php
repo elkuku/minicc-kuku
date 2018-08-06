02e591748c257fb062918d72c4f285d50f16e8a1
Nikolai Plath <der.el.kuku@gmail.com>
2018-08-06 10:12:55 -0500

    Add git status indicator

---
+++ src/DataCollector/GitDataCollector.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: test
+ * Date: 08/07/18
+ * Time: 18:03
+ */
+
+namespace App\DataCollector;
+
+use App\Helper\GitLoader;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\DataCollector\DataCollector;
+
+class GitDataCollector extends DataCollector
+{
+	private $gitLoader;
+
+	public function __construct(GitLoader $gitLoader)
+	{
+		$this->gitLoader = $gitLoader;
+	}
+
+	public function collect(Request $request, Response $response, \Exception $exception = null)
+	{
+		$this->data = [
+			'git_branch' => $this->gitLoader->getBranchName(),
+			'last_commit_message' => $this->gitLoader->getLastCommitMessage(),
+			'logs' => $this->gitLoader->getLastCommitDetail(),
+		];
+	}
+
+	public function getName()
+	{
+		return 'app.git_data_collector';
+	}
+
+	public function reset()
+	{
+		$this->data = array();
+	}
+
+	public function getGitBranch()
+	{
+		return $this->data['git_branch'];
+	}
+
+	public function getLastCommitMessage()
+	{
+		return $this->data['last_commit_message'];
+	}
+
+	public function getLastCommitAuthor()
+	{
+		return $this->data['logs']['author'];
+	}
+
+	public function getLastCommitDate()
+	{
+		return $this->data['logs']['date'];
+	}
+	public function getSha()
+	{
+		return $this->data['logs']['sha'];
+	}
+}
---
+++ src/Helper/GitLoader.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: test
+ * Date: 08/07/18
+ * Time: 18:00
+ */
+
+namespace App\Helper;
+
+class GitLoader
+{
+	private $projectDir;
+
+	public function __construct($projectDir)
+	{
+		$this->projectDir = $projectDir;
+	}
+
+	public function getBranchName(): string
+	{
+		$gitHeadFile = $this->projectDir . '/.git/HEAD';
+		$branchname  = 'no branch name';
+
+		$stringFromFile = file_exists($gitHeadFile) ? file($gitHeadFile, FILE_USE_INCLUDE_PATH) : '';
+
+		if ($stringFromFile !== 0 && \is_array($stringFromFile))
+		{
+			// Get the string from the array
+			$firstLine = $stringFromFile[0];
+
+			// Seperate out by the "/" in the string
+			$explodedString = explode('/', $firstLine, 3);
+
+			$branchname = trim($explodedString[2]);
+		}
+
+		return $branchname;
+	}
+
+	public function getLastCommitMessage(): string
+	{
+		$gitCommitMessageFile = $this->projectDir . '/.git/COMMIT_EDITMSG';
+		$commitMessage        = file_exists($gitCommitMessageFile) ? file($gitCommitMessageFile, FILE_USE_INCLUDE_PATH) : '';
+
+		return \is_array($commitMessage) ? trim($commitMessage[0]) : '';
+	}
+
+	public function getLastCommitDetail(): array
+	{
+		$gitLogFile = $this->projectDir . '/.git/logs/HEAD';
+		$gitLogs    = file_exists($gitLogFile) ? file($gitLogFile, FILE_USE_INCLUDE_PATH) : '';
+		$sha = trim($this->execCommand('cd ' . $this->projectDir . ' && git rev-parse --short HEAD'));
+
+		preg_match("/([\w]+) ([\w]+) ([\w\s]+) (<[\w\.@]+>) ([\d]+) ([\d-]+)\tcommit: ([\w\s]+)/", end($gitLogs), $matches);
+
+		$logs = [];
+
+		$logs['author'] = $matches[3] ?? 'not defined';
+		$logs['date']   = isset($matches[5]) ? date('Y/m/d H:i', $matches[5]) : 'not defined';
+		$logs['sha'] = $sha;
+
+		return $logs;
+	}
+
+	/**
+	 * Execute a command on the server.
+	 *
+	 * @param   string  $command  The command to execute.
+	 *
+	 * @return string
+	 *
+	 * @throws \RuntimeException
+	 */
+	protected function execCommand($command): string
+	{
+		ob_start();
+		$lastLine = system($command, $status);
+
+		if ($status)
+		{
+			// Command exited with a status != 0
+			if ($lastLine)
+			{
+				throw new \RuntimeException($lastLine);
+			}
+
+			throw new \RuntimeException('An unknown error occurred');
+		}
+
+		ob_end_clean();
+
+		return $lastLine;
+	}
+}
