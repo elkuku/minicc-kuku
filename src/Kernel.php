9576a8c3fdd0751ee11beaed339348270d54f5b9
Nikolai Plath <der.el.kuku@gmail.com>
2019-12-30 10:09:12 -0500

    composer recipes:install symfony/framework-bundle --force -v

--- src/Kernel.php
+++ src/Kernel.php
@@ -4,69 +4,51 @@ namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 use Symfony\Component\Routing\RouteCollectionBuilder;
 
 class Kernel extends BaseKernel
 {
-	use MicroKernelTrait;
+    use MicroKernelTrait;
 
-	const CONFIG_EXTS = '.{php,xml,yaml,yml}';
+    private const CONFIG_EXTS = '.{php,xml,yaml,yml}';
 
-	public function getCacheDir()
-	{
-		return $this->getProjectDir() . '/var/cache/' . $this->environment;
-	}
+    public function registerBundles(): iterable
+    {
+        $contents = require $this->getProjectDir().'/config/bundles.php';
+        foreach ($contents as $class => $envs) {
+            if ($envs[$this->environment] ?? $envs['all'] ?? false) {
+                yield new $class();
+            }
+        }
+    }
 
-	public function getLogDir()
-	{
-		return $this->getProjectDir() . '/var/log';
-	}
+    public function getProjectDir(): string
+    {
+        return \dirname(__DIR__);
+    }
 
-	public function registerBundles()
-	{
-		$contents = require $this->getProjectDir() . '/config/bundles.php';
+    protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader): void
+    {
+        $container->addResource(new FileResource($this->getProjectDir().'/config/bundles.php'));
+        $container->setParameter('container.dumper.inline_class_loader', \PHP_VERSION_ID < 70400 || $this->debug);
+        $container->setParameter('container.dumper.inline_factories', true);
+        $confDir = $this->getProjectDir().'/config';
 
-		foreach ($contents as $class => $envs)
-		{
-			if (isset($envs['all']) || isset($envs[$this->environment]))
-			{
-				yield new $class;
-			}
-		}
-	}
+        $loader->load($confDir.'/{packages}/*'.self::CONFIG_EXTS, 'glob');
+        $loader->load($confDir.'/{packages}/'.$this->environment.'/*'.self::CONFIG_EXTS, 'glob');
+        $loader->load($confDir.'/{services}'.self::CONFIG_EXTS, 'glob');
+        $loader->load($confDir.'/{services}_'.$this->environment.self::CONFIG_EXTS, 'glob');
+    }
 
-	protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader)
-	{
-		$container->setParameter('container.autowiring.strict_mode', true);
-		$container->setParameter('container.dumper.inline_class_loader', true);
-		$confDir = $this->getProjectDir() . '/config';
-		$loader->load($confDir . '/packages/*' . self::CONFIG_EXTS, 'glob');
+    protected function configureRoutes(RouteCollectionBuilder $routes): void
+    {
+        $confDir = $this->getProjectDir().'/config';
 
-		if (is_dir($confDir . '/packages/' . $this->environment))
-		{
-			$loader->load($confDir . '/packages/' . $this->environment . '/**/*' . self::CONFIG_EXTS, 'glob');
-		}
-
-		$loader->load($confDir . '/services' . self::CONFIG_EXTS, 'glob');
-		$loader->load($confDir . '/services_' . $this->environment . self::CONFIG_EXTS, 'glob');
-	}
-
-	protected function configureRoutes(RouteCollectionBuilder $routes)
-	{
-		$confDir = $this->getProjectDir() . '/config';
-
-		if (is_dir($confDir . '/routes/'))
-		{
-			$routes->import($confDir . '/routes/*' . self::CONFIG_EXTS, '/', 'glob');
-		}
-
-		if (is_dir($confDir . '/routes/' . $this->environment))
-		{
-			$routes->import($confDir . '/routes/' . $this->environment . '/**/*' . self::CONFIG_EXTS, '/', 'glob');
-		}
-
-		$routes->import($confDir . '/routes' . self::CONFIG_EXTS, '/', 'glob');
-	}
+        $routes->import($confDir.'/{routes}/'.$this->environment.'/*'.self::CONFIG_EXTS, '/', 'glob');
+        $routes->import($confDir.'/{routes}/*'.self::CONFIG_EXTS, '/', 'glob');
+        $routes->import($confDir.'/{routes}'.self::CONFIG_EXTS, '/', 'glob');
+    }
 }
