8367bad846a649bb10ce7f9406bde5ab8d0184db
Nikolai Plath <der.el.kuku@gmail.com>
2020-01-03 10:02:21 -0500

    Add G login

---
+++ src/Security/GoogleAuthenticator.php
@@ -0,0 +1,157 @@
+<?php
+
+namespace App\Security;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use KnpU\OAuth2ClientBundle\Security\Authenticator\SocialAuthenticator;
+use KnpU\OAuth2ClientBundle\Client\Provider\GoogleClient;
+use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
+use League\OAuth2\Client\Provider\GoogleUser;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Http\Util\TargetPathTrait;
+
+class GoogleAuthenticator extends SocialAuthenticator
+{
+	use TargetPathTrait;
+
+	/**
+	 * @var ClientRegistry
+	 */
+	private $clientRegistry;
+
+	/**
+	 * @var EntityManagerInterface
+	 */
+	private $em;
+
+	/**
+	 * @var UserRepository
+	 */
+	private $userRepository;
+	/**
+	 * @var UrlGeneratorInterface
+	 */
+	private $urlGenerator;
+
+	public function __construct(ClientRegistry $clientRegistry, EntityManagerInterface $em, UserRepository $userRepository, UrlGeneratorInterface $urlGenerator)
+	{
+		$this->clientRegistry = $clientRegistry;
+		$this->em = $em;
+		$this->userRepository = $userRepository;
+		$this->urlGenerator = $urlGenerator;
+	}
+
+	/**
+	 * @param Request $request
+	 *
+	 * @return bool
+	 */
+	public function supports(Request $request)
+	{
+		// continue ONLY if the current ROUTE matches the check ROUTE
+		return $request->attributes->get('_route') === 'connect_google_check';
+	}
+
+	/**
+	 * @param Request $request
+	 *
+	 * @return \League\OAuth2\Client\Token\AccessToken|mixed
+	 */
+	public function getCredentials(Request $request)
+	{
+		// this method is only called if supports() returns true
+
+		return $this->fetchAccessToken($this->getGoogleClient());
+	}
+
+	/**
+	 * @param mixed                 $credentials
+	 * @param UserProviderInterface $userProvider
+	 *
+	 * @return User|null|object|\Symfony\Component\Security\Core\User\UserInterface
+	 */
+	public function getUser($credentials, UserProviderInterface $userProvider)
+	{
+		/** @var GoogleUser $googleUser */
+		$googleUser = $this->getGoogleClient()
+			->fetchUserFromToken($credentials);
+
+		$email = $googleUser->getEmail();
+
+		// 1) have they logged in with Google before? Easy!
+		$user = $this->userRepository->findOneBy(['email' => $googleUser->getEmail()]);
+
+		if (!$user) {
+			$user = new User();
+			$user->setEmail($email);
+
+			$this->em->persist($user);
+			$this->em->flush();
+		}
+
+		return $user;
+	}
+
+	/**
+	 * @return GoogleClient
+	 */
+	private function getGoogleClient()
+	{
+		return $this->clientRegistry->getClient('google');
+	}
+
+	/**
+	 * @param Request        $request
+	 * @param TokenInterface $token
+	 * @param string         $providerKey
+	 *
+	 * @return null|Response
+	 */
+	public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
+	{
+		if ($targetPath = $this->getTargetPath($request->getSession(), $providerKey)) {
+			return new RedirectResponse($targetPath);
+		}
+
+		// For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
+		return new RedirectResponse($this->urlGenerator->generate('default'));
+	}
+
+	/**
+	 * @param Request                 $request
+	 * @param AuthenticationException $exception
+	 *
+	 * @return null|Response
+	 */
+	public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
+	{
+		$message = strtr($exception->getMessageKey(), $exception->getMessageData());
+
+		return new Response($message, Response::HTTP_FORBIDDEN);
+	}
+
+	/**
+	 * Called when authentication is needed, but it's not sent.
+	 * This redirects to the 'login'.
+	 *
+	 * @param Request                      $request
+	 * @param AuthenticationException|null $authException
+	 *
+	 * @return RedirectResponse
+	 */
+	public function start(Request $request, AuthenticationException $authException = null)
+	{
+		return new RedirectResponse(
+			'/connect/', // might be the site, where users choose their oauth provider
+			Response::HTTP_TEMPORARY_REDIRECT
+		);
+	}
+}
