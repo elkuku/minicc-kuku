33f3c154bc67558f5554f7b5fdf776659e52d981
elkuku <der.el.kuku@gmail.com>
2020-06-23 15:54:49 -0500

    Formatting

--- src/Controller/Admin/TasksController.php
+++ src/Controller/Admin/TasksController.php
@@ -23,96 +23,98 @@ use Symfony\Component\HttpFoundation\Response;
  */
 class TasksController extends AbstractController
 {
-	/**
-	 * @Route("/admin-tasks", name="admin-tasks")
-	 * @Security("is_granted('ROLE_ADMIN')")
-	 *
-	 * @return Response
-	 */
-	public function index(): Response
-	{
-		return $this->render('admin/tasks.html.twig',
-			[
-				'currentYear' => (int) date('Y'),
-			]
-		);
-	}
+    /**
+     * @Route("/admin-tasks", name="admin-tasks")
+     * @Security("is_granted('ROLE_ADMIN')")
+     *
+     * @return Response
+     */
+    public function index(): Response
+    {
+        return $this->render(
+            'admin/tasks.html.twig',
+            [
+                'currentYear' => (int)date('Y'),
+            ]
+        );
+    }
 
-	/**
-	 * @Route("/console-view/{item}", name="console-view")
-	 * @Security("is_granted('ROLE_ADMIN')")
-	 *
-	 * @param string  $item
-	 * @param Request $request
-	 *
-	 * @return Response
-	 * @throws \Exception
-	 */
-	public function consoleView(string $item, Request $request, KernelInterface $kernel): Response
-	{
-		$command = [];
+    /**
+     * @Route("/console-view/{item}", name="console-view")
+     * @Security("is_granted('ROLE_ADMIN')")
+     *
+     * @param string  $item
+     * @param Request $request
+     *
+     * @return Response
+     * @throws \Exception
+     */
+    public function consoleView(string $item, Request $request, KernelInterface $kernel): Response
+    {
+        $command = [];
 
-		switch ($item)
-		{
-			case 'routes':
-				$command['command'] = 'debug:router';
-				break;
-			case 'route-match':
-				$command['command']   = 'router:match';
-				$command['path_info'] = $request->request->get('route');
-				break;
-			case 'migrations':
-				$command['command'] = 'doctrine:migrations:status';
-				break;
-			case 'security':
-				$command['command']  = 'security:check';
-				$command['lockfile'] = \dirname($kernel->getRootDir());
-				break;
-			default:
-				throw new \UnexpectedValueException('Unknown command');
-		}
+        switch ($item) {
+            case 'routes':
+                $command['command'] = 'debug:router';
+                break;
+            case 'route-match':
+                $command['command'] = 'router:match';
+                $command['path_info'] = $request->request->get('route');
+                break;
+            case 'migrations':
+                $command['command'] = 'doctrine:migrations:status';
+                break;
+            case 'security':
+                $command['command'] = 'security:check';
+                $command['lockfile'] = \dirname($kernel->getRootDir());
+                break;
+            default:
+                throw new \UnexpectedValueException('Unknown command');
+        }
 
-		$application = new Application($kernel);
-		$application->setAutoExit(false);
+        $application = new Application($kernel);
+        $application->setAutoExit(false);
 
-		$input  = new ArrayInput($command);
-		$output = new BufferedOutput;
+        $input = new ArrayInput($command);
+        $output = new BufferedOutput;
 
-		$application->run($input, $output);
+        $application->run($input, $output);
 
-		return $this->render(
-			'admin/tasks.html.twig',
-			[
-				'consoleCommand' => $command,
-				'consoleOutput'  => $output->fetch(),
-			]
-		);
-	}
+        return $this->render(
+            'admin/tasks.html.twig',
+            [
+                'consoleCommand' => $command,
+                'consoleOutput'  => $output->fetch(),
+            ]
+        );
+    }
 
-	/**
-	 * @Route("/sysinfo", name="sysinfo")
-	 * @Security("is_granted('ROLE_ADMIN')")
-	 *
-	 * @return Response
-	 */
-	public function sysInfo(KernelInterface $kernel): Response
-	{
-		$application = new Application($kernel);
-		$application->setAutoExit(false);
+    /**
+     * @Route("/sysinfo", name="sysinfo")
+     * @Security("is_granted('ROLE_ADMIN')")
+     *
+     * @return Response
+     */
+    public function sysInfo(KernelInterface $kernel): Response
+    {
+        $application = new Application($kernel);
+        $application->setAutoExit(false);
 
-		$input = new ArrayInput([
-			'command' => 'about',
-		]);
+        $input = new ArrayInput(
+            [
+                'command' => 'about',
+            ]
+        );
 
-		$output = new BufferedOutput();
+        $output = new BufferedOutput();
 
-		$application->run($input, $output);
+        $application->run($input, $output);
 
-		return $this->render(
-			'admin/sysinfo.html.twig',
-			[
-				'info' => $output->fetch(),
-			]
-		);
-	}
+        return $this->render(
+            'admin/sysinfo.html.twig',
+            [
+                'info' => $output->fetch(),
+            ]
+        );
+    }
 }
