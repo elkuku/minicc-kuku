4e88ac903d61d333015dcbc76005b220b8a1bafd
Nikolai Plath <der.el.kuku@gmail.com>
2021-02-15 08:35:28 -0500

    Update tests

--- tests/DataFixtureTestCase.php
+++ tests/DataFixtureTestCase.php
@@ -24,7 +24,7 @@ class DataFixtureTestCase extends WebTestCase
     /**
      * {@inheritDoc}
      */
-    protected function setUp()
+    protected function setUp(): void
     {
         parent::setUp();
 
