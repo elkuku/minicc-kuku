9f6182e92b61c2f2b243c4e8c0cdbde6fac24b43
Nikolai Plath <der.el.kuku@gmail.com>
2021-02-16 11:06:53 -0500

    Update tests

--- tests/Controller/ControllerAccessTest.php
+++ tests/Controller/ControllerAccessTest.php
@@ -1,80 +1,50 @@
 <?php
 
-namespace App\Tests\Controller;
+namespace Controller;
 
-use App\Tests\FixtureAwareTestCase;
-use App\Tests\Fixtures\ContractFixture;
-use App\Tests\Fixtures\PaymentMethodFixture;
-use App\Tests\Fixtures\StoreFixture;
-use App\Tests\Fixtures\TransactionFixture;
-use App\Tests\Fixtures\UserFixture;
 use DirectoryIterator;
 use Exception;
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class ControllerAccessTest extends FixtureAwareTestCase
+/**
+ * Controller "smoke" test
+ */
+class ControllerAccessTest extends WebTestCase
 {
     private $routeLoader;
 
     private array $exceptions
         = [
             'welcome' => [
-                'expected' => 200,
+                'statusCode' => 200,
             ],
             'about'   => [
-                'expected' => 200,
+                'statusCode' => 200,
             ],
             'contact' => [
-                'expected' => 200,
+                'statusCode' => 200,
             ],
-            'login' => [
-                'expected' => 200,
+            'login'   => [
+                'statusCode' => 200,
             ],
         ];
 
-    protected KernelBrowser $client;
-
-    protected function setUp(): void
-    {
-        $this->client = static::createClient();
-        parent::setUp();
-        $kernel = static::bootKernel();
-
-        $this->addFixture(new StoreFixture());
-        $this->addFixture(new TransactionFixture());
-        $this->addFixture(new PaymentMethodFixture());
-        $this->addFixture(new ContractFixture());
-        $this->addFixture(new UserFixture());
-
-        $this->executeFixtures();
-
-        $this->routeLoader = $kernel->getContainer()->get('routing.loader');
-    }
-
     /**
      * @throws Exception
      */
-    private function loadRoutes($controllerName)
+    public function testRoutes(): void
     {
-        $routerClass = 'App\Controller\\'.$controllerName;
+        $client = static::createClient();
+        $routeLoader = static::bootKernel()->getContainer()
+            ->get('routing.loader');
 
-        if (class_exists($routerClass)) {
-            return $this->routeLoader->load($routerClass);
-        }
-
-        return false;
-    }
-
-    /**
-     * @throws Exception
-     */
-    public function testShowPage(): void
-    {
-        $path = __DIR__.'/../../src/Controller';
-
-        foreach (new DirectoryIterator($path) as $item) {
+        foreach (
+            new DirectoryIterator(__DIR__.'/../../src/Controller') as $item
+        ) {
             if (
                 $item->isDot()
+                || $item->isDir()
                 || in_array(
                     $item->getBasename(),
                     ['.gitignore', 'GoogleController.php']
@@ -83,63 +53,177 @@ class ControllerAccessTest extends FixtureAwareTestCase
                 continue;
             }
 
-            if ('TransactionController.php' === $item->getBasename()) {
-                // @todo transactions is not a valid table name in SQLite :(
-                continue;
-            }
+            $routerClass = 'App\Controller\\'.basename(
+                    $item->getBasename(),
+                    '.php'
+                );
+            $routes = $routeLoader->load($routerClass)->all();
 
-            $controllerName = basename($item->getBasename(), '.php');
+            $this->processRoutes($routes, $client);
+        }
+    }
 
-            $r = $this->loadRoutes($controllerName);
-
-            if (!$r) {
-                continue;
-            }
-
-            $routes = $r->all();
-
-            foreach ($routes as $routeName => $route) {
-                $method = 'GET';
-                $defaultId = 1;
-                $defaultExpected = 302;
-
-                if (array_key_exists($routeName, $this->exceptions)) {
-                    if (array_key_exists(
-                        'method',
-                        $this->exceptions[$routeName]
-                    )
-                    ) {
-                        $method = $this->exceptions[$routeName]['method'];
-                    }
-                    if (array_key_exists(
-                        'expected',
-                        $this->exceptions[$routeName]
-                    )
-                    ) {
-                        $defaultExpected = $this->exceptions[$routeName]['expected'];
-                    }
-                    if (array_key_exists(
-                        'params',
-                        $this->exceptions[$routeName]
-                    )
-                    ) {
-                        $params = $this->exceptions[$routeName]['params'];
-                        if (array_key_exists('id', $params)) {
-                            $defaultId = $params['id'];
-                        }
+    private function processRoutes(array $routes, KernelBrowser $browser): void
+    {
+        foreach ($routes as $routeName => $route) {
+            $defaultId = 1;
+            $expectedStatusCode = 302;
+            if (array_key_exists($routeName, $this->exceptions)) {
+                if (array_key_exists(
+                    'statusCode',
+                    $this->exceptions[$routeName]
+                )
+                ) {
+                    $expectedStatusCode = $this->exceptions[$routeName]['statusCode'];
+                }
+                if (array_key_exists('params', $this->exceptions[$routeName])) {
+                    $params = $this->exceptions[$routeName]['params'];
+                    if (array_key_exists('id', $params)) {
+                        $defaultId = $params['id'];
                     }
                 }
+            }
 
-                $path = $route->getPath();
-                $path = str_replace('{id}', $defaultId, $path);
-                // echo 'Testing: '.$path.PHP_EOL;
-                $this->client->request($method, $path);
+            $methods = $route->getMethods() ?: ['GET'];
+
+            // WTF start
+            if (isset($methods[0]) && strpos($methods[0], '|')) {
+                // The first element contains a "|" character - WTF??
+                $methods = explode('|', $methods[0]);
+            }
+            // WTF end
+
+            $path = str_replace('{id}', $defaultId, $route->getPath());
+            foreach ($methods as $method) {
+                $browser->request($method, $path);
+                if (true) {
+                    echo sprintf(
+                            'Testing: %s - %s Expected: %s got: %s',
+                            $method,
+                            $path,
+                            $expectedStatusCode,
+                            $browser->getResponse()->getStatusCode()
+                        ).PHP_EOL;
+                }
                 self::assertEquals(
-                    $defaultExpected,
-                    $this->client->getResponse()->getStatusCode(),
+                    $expectedStatusCode,
+                    $browser->getResponse()->getStatusCode(),
                     sprintf('failed: %s (%s)', $routeName, $path)
                 );
             }
         }
     }
+
+    // protected KernelBrowser $client;
+    //
+    // protected function setUp(): void
+    // {
+    //     $this->client = static::createClient();
+    //     // parent::setUp();
+    //     // $kernel = static::bootKernel();
+    //
+    //     // $this->addFixture(new StoreFixture());
+    //     // $this->addFixture(new TransactionFixture());
+    //     // $this->addFixture(new PaymentMethodFixture());
+    //     // $this->addFixture(new ContractFixture());
+    //     // $this->addFixture(new UserFixture());
+    //     //
+    //     // $this->executeFixtures();
+    //
+    //     // $this->routeLoader = $kernel->getContainer()->get('routing.loader');
+    //     $this->routeLoader = static::bootKernel()->getContainer()
+    //         ->get('routing.loader');
+    // }
+    //
+    // /**
+    //  * @throws Exception
+    //  */
+    // private function loadRoutes($controllerName)
+    // {
+    //     $routerClass = 'App\Controller\\'.$controllerName;
+    //
+    //     if (class_exists($routerClass)) {
+    //         return $this->routeLoader->load($routerClass);
+    //     }
+    //
+    //     return false;
+    // }
+    //
+    // /**
+    //  * @throws Exception
+    //  */
+    // public function testShowPage(): void
+    // {
+    //     $path = __DIR__.'/../../src/Controller';
+    //
+    //     foreach (new DirectoryIterator($path) as $item) {
+    //         if (
+    //             $item->isDot()
+    //             || in_array(
+    //                 $item->getBasename(),
+    //                 ['.gitignore', 'GoogleController.php']
+    //             )
+    //         ) {
+    //             continue;
+    //         }
+    //
+    //         if ('TransactionController.php' === $item->getBasename()) {
+    //             // @todo transactions is not a valid table name in SQLite :(
+    //             continue;
+    //         }
+    //
+    //         $controllerName = basename($item->getBasename(), '.php');
+    //
+    //         $r = $this->loadRoutes($controllerName);
+    //
+    //         if (!$r) {
+    //             continue;
+    //         }
+    //
+    //         $routes = $r->all();
+    //
+    //         foreach ($routes as $routeName => $route) {
+    //             $method = 'GET';
+    //             $defaultId = 1;
+    //             $defaultExpected = 302;
+    //
+    //             if (array_key_exists($routeName, $this->exceptions)) {
+    //                 if (array_key_exists(
+    //                     'method',
+    //                     $this->exceptions[$routeName]
+    //                 )
+    //                 ) {
+    //                     $method = $this->exceptions[$routeName]['method'];
+    //                 }
+    //                 if (array_key_exists(
+    //                     'expected',
+    //                     $this->exceptions[$routeName]
+    //                 )
+    //                 ) {
+    //                     $defaultExpected = $this->exceptions[$routeName]['expected'];
+    //                 }
+    //                 if (array_key_exists(
+    //                     'params',
+    //                     $this->exceptions[$routeName]
+    //                 )
+    //                 ) {
+    //                     $params = $this->exceptions[$routeName]['params'];
+    //                     if (array_key_exists('id', $params)) {
+    //                         $defaultId = $params['id'];
+    //                     }
+    //                 }
+    //             }
+    //
+    //             $path = $route->getPath();
+    //             $path = str_replace('{id}', $defaultId, $path);
+    //             // echo 'Testing: '.$path.PHP_EOL;
+    //             $this->client->request($method, $path);
+    //             self::assertEquals(
+    //                 $defaultExpected,
+    //                 $this->client->getResponse()->getStatusCode(),
+    //                 sprintf('failed: %s (%s)', $routeName, $path)
+    //             );
+    //         }
+    //     }
+    // }
 }
---
+++ tests/Twig/TwigExtensionTest.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace Twig;
+
+use App\Entity\User;
+use App\Twig\AppExtension;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class TwigExtensionTest extends WebTestCase
+{
+    private AppExtension $twigExtension;
+
+    public function setUp(): void
+    {
+        static::createClient();
+        $this->twigExtension = new AppExtension(self::$container);
+    }
+
+    public function testPriceFilter(): void
+    {
+        self::assertSame(
+            '<span class="amount">112.00</span>',
+            $this->twigExtension->priceFilter(112)
+        );
+        self::assertSame(
+            '<span class="amount amount-red">-112.00</span>',
+            $this->twigExtension->priceFilter(-112)
+        );
+    }
+
+    public function testX(): void
+    {
+        self::assertSame(112.0, $this->twigExtension->conIvaFilter(100));
+        self::assertSame(12.0, $this->twigExtension->taxFromTotalFilter(112));
+    }
+
+    public function testFormatRuc(): void
+    {
+        $user = (new User())
+            ->setInqCi('123456789-6');
+
+        self::assertSame(
+            '123 456 789 6',
+            $this->twigExtension->formatRUC($user)
+        );
+
+        $user = (new User())
+            ->setInqRuc('1234567896001');
+
+        self::assertSame(
+            '123 456 789 6 001',
+            $this->twigExtension->formatRUC($user)
+        );
+
+        $user = (new User())
+            ->setInqRuc('12345678961');
+
+        self::assertSame(
+            '123 456 789 61',
+            $this->twigExtension->formatRUC($user)
+        );
+    }
+
+    public function testIntlDate(): void
+    {
+        self::assertSame(
+            '15 de septiembre 1966',
+            $this->twigExtension->intlDate('1966-09-15')
+        );
+
+        self::assertSame(
+            '15 de September 1966',
+            $this->twigExtension->intlDate('1966-09-15', lang: 'de_DE')
+        );
+
+        self::assertSame(
+            '15 September 1966',
+            $this->twigExtension->intlDate('1966-09-15', 'd MMMM YYYY', 'de_DE')
+        );
+
+        self::assertSame(
+            'INVALID',
+            $this->twigExtension->intlDate('INVALID')
+        );
+    }
+
+    public function testShortName(): void
+    {
+        self::assertSame(
+            'Juan Perez',
+            $this->twigExtension->shortName('Juan Perez')
+        );
+
+        self::assertSame(
+            'Juan Perez',
+            $this->twigExtension->shortName('Juan José Perez')
+        );
+
+        self::assertSame(
+            'Juan Perez',
+            $this->twigExtension->shortName('Juan José Perez Pillo')
+        );
+
+        self::assertSame(
+            'Juan',
+            $this->twigExtension->shortName('Juan')
+        );
+    }
+}
