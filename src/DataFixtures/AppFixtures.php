e65b51bd61531b82f8e944d37068a6064eda5266
Nikolai Plath <der.el.kuku@gmail.com>
2021-02-16 11:11:16 -0500

    Update fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,9 @@
 
 namespace App\DataFixtures;
 
+use _HumbugBox5d215ba2066e\Nette\Utils\DateTime;
+use App\Entity\Store;
+use App\Entity\Transaction;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +12,13 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $store = (new Store())
+            ->setValAlq(123);
+        $manager->persist($store);
+
+        $transaction = (new Transaction())
+            ->setDate(new DateTime());
+        $manager->persist($transaction);
 
         $manager->flush();
     }
