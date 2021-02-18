e7703c21d145ca71843f1d1a6305a3dbb6dd2be8
Nikolai Plath <der.el.kuku@gmail.com>
2021-02-18 10:51:48 -0500

    Add store access test

---
+++ tests/StoreAccessTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Tests;
+
+use App\Repository\UserRepository;
+use PHPUnit\Framework\ExpectationFailedException;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class StoreAccessTest extends WebTestCase
+{
+    public function testAnonymousAccess(): void
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/stores/1');
+        self::assertResponseRedirects();
+        $crawler = $client->followRedirect();
+
+        try {
+            self::assertSelectorTextContains('h2', 'Login');
+        } catch (ExpectationFailedException $exception) {
+            file_put_contents('gugu.html', $crawler->html());
+
+            throw $exception;
+        }
+    }
+
+    public function testAccessWrongUser(): void
+    {
+        $client = static::createClient();
+        $userRepository = static::$container->get(UserRepository::class);
+        $testUser = $userRepository->findOneByEmail('user2@example.com');
+        $client->loginUser($testUser);
+
+        $crawler = $client->request('GET', '/stores/1');
+        try {
+            self::assertSelectorTextContains('small', 'Forbidden');
+        } catch (ExpectationFailedException $exception) {
+            file_put_contents('guguxx.html', $crawler->html());
+
+            throw $exception;
+        }
+    }
+
+    public function testAccess(): void
+    {
+        $client = static::createClient();
+        $userRepository = static::$container->get(UserRepository::class);
+        $testUser = $userRepository->findOneByEmail('user1@example.com');
+        $client->loginUser($testUser);
+
+        $crawler = $client->request('GET', '/stores/1');
+        try {
+            self::assertSelectorTextContains('h2', 'Transacciones');
+        } catch (ExpectationFailedException $exception) {
+            file_put_contents('guguyy.html', $crawler->html());
+
+            throw $exception;
+        }
+    }
+}
