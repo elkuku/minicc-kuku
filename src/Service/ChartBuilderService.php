25e8ea6598fdfd4ac3011791f4fbe415b40081f6
Nikolai Plath <der.el.kuku@gmail.com>
2021-03-01 12:48:19 -0500

    Move methods

---
+++ src/Service/ChartBuilderService.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
+use Symfony\UX\Chartjs\Model\Chart;
+
+class ChartBuilderService
+{
+    public function __construct(private ChartBuilderInterface $chartBuilder)
+    {
+    }
+
+    public function getDashboardChart(
+        string $title,
+        array $labels,
+        array $data
+    ): Chart {
+        $bgColors = [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+        ];
+
+        $borderColors = [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)',
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+        ];
+
+        $chart = $this->chartBuilder->createChart(Chart::TYPE_BAR);
+        $chart->setData(
+            [
+                'labels'   => $labels,
+                'datasets' => [
+                    [
+                        'label'           => $title,
+                        'backgroundColor' => $bgColors,
+                        'borderColor'     => $borderColors,
+                        'borderWidth'     => 1,
+                        'data'            => $data,
+                    ],
+                ],
+            ]
+        );
+
+        return $chart;
+    }
+
+    public function getStoreChart(
+        array $labels,
+        array $dataPayments,
+        array $dataRent
+    ): Chart {
+        $chart = $this->chartBuilder->createChart(Chart::TYPE_LINE);
+        $chart->setData(
+            [
+                'labels'   => $labels,
+                'datasets' => [
+                    [
+                        'label'           => 'Pagos',
+                        'data'            => $dataPayments,
+                        'fill'            => 'false',
+                        'lineTension'     => 0.1,
+                        'backgroundColor' => 'rgba(75,192,192,0.4)',
+                        'borderColor'     => 'rgba(75,192,192,1)',
+                    ],
+                    [
+                        'label'           => 'Alquiler',
+                        'data'            => $dataRent,
+                        'backgroundColor' => 'rgba(255, 206, 86, 0.2)',
+                        'borderColor'     => 'rgba(255, 206, 86, 0.2)',
+                        'borderWidth'     => 1,
+                    ],
+                ],
+            ]
+        );
+
+        return $chart;
+    }
+}
--- src/Service/PDFHelper.php
+++ src/Service/PDFHelper.php
@@ -8,14 +8,67 @@
 
 namespace App\Service;
 
+use App\Entity\Store;
+use App\Entity\Transaction;
+use App\Repository\TransactionRepository;
+use Twig\Environment;
+
 class PDFHelper
 {
-    public function __construct(private string $rootDir)
-    {
+    public function __construct(
+        private string $rootDir,
+        private Environment $twig
+    ) {
     }
 
     public function getRoot(): string
     {
         return $this->rootDir;
+    }
+
+    public function renderTransactionHtml(
+        TransactionRepository $transactionRepository,
+        Store $store,
+        int $year,
+        int $transactionsPerPage = 42
+    ): string {
+        $transactions = $transactionRepository->findByStoreAndYear(
+            $store,
+            $year
+        );
+
+        $pages = (int)(count($transactions) / $transactionsPerPage) + 1;
+        $fillers = $transactionsPerPage - (count($transactions) - ($pages - 1)
+                * $transactionsPerPage);
+
+        for ($i = 1; $i < $fillers; $i++) {
+            $transaction = new Transaction;
+            $transactions[] = $transaction;
+        }
+
+        return $this->twig->render(
+            '_pdf/transactions-pdf.html.twig',
+            [
+                'saldoAnterior' => $transactionRepository->getSaldoAnterior(
+                    $store,
+                    $year
+                ),
+                'transactions'  => $transactions,
+                'store'         => $store,
+                'year'          => $year,
+            ]
+        );
+    }
+
+    public function renderPayrollsHtml(
+        int $year,
+        int $month,
+        PayrollHelper $payrollHelper,
+        int $storeId = 0
+    ): string {
+        return $this->twig->render(
+            '_pdf/payrolls-pdf.html.twig',
+            $payrollHelper->getData($year, $month, $storeId)
+        );
     }
 }
