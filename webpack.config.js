cbaa3b2f3d0f6e9e8c1558662ac54e0970eded7e
elkuku <der.el.kuku@gmail.com>
2024-04-12 11:33:24 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -21,10 +21,6 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
-    .addEntry('js/default/background', './assets/js/default/background.js')
-    .addEntry('js/transaction/form', './assets/js/transaction/form.js')
-
-    .addEntry('admin', './assets/admin.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
