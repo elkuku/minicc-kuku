37685f3512ce2d3c14528dd20dd6638033d1d3b8
elkuku <der.el.kuku@gmail.com>
2024-06-27 09:36:00 -0500

    Add Twig extension runtime test class

---
+++ tests/Twig/TwigExtensionRuntimeTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Tests\Twig;
+
+use App\Repository\UserRepository;
+use App\Service\ShaFinder;
+use App\Service\TaxService;
+use App\Twig\Runtime\AppExtensionRuntime;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class TwigExtensionRuntimeTest extends WebTestCase
+{
+    private AppExtensionRuntime $twigExtensionRuntime;
+
+    public function setUp(): void
+    {
+        static::createClient();
+        $this->twigExtensionRuntime = new AppExtensionRuntime(
+            $this->createMock(UserRepository::class),
+            $this->createMock(ShaFinder::class),
+            new TaxService(12),
+        );
+    }
+
+    public function testX(): void
+    {
+        self::assertSame(112.0, $this->twigExtensionRuntime->getValueWithTax(100));
+        self::assertSame(12.0, $this->twigExtensionRuntime->getTaxFromTotal(112));
+    }
+}
