353bbeb5ac10f3e4fdb119e970e7c6f8e0fa785d
elkuku <der.el.kuku@gmail.com>
2024-12-13 06:33:06 -0500

    Update recipe for symfony/stimulus-bundle

--- assets/app.js
+++ assets/app.js
@@ -1,8 +1,6 @@
+import './bootstrap.js';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'open-iconic/font/css/open-iconic-bootstrap.css';
-//import 'bootstrap';
 
 import './styles/app.css';
-
-// start the Stimulus application
-import './bootstrap.js';
