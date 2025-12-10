8aa771a8af98b6ae09b241c44d70e65a47db84c4
elkuku <der.el.kuku@gmail.com>
2025-12-10 05:51:37 -0500

    Rename stimulus_bootstrap.js

--- assets/bootstrap.js
+++
@@ -1,6 +0,0 @@
-import { startStimulusApp } from '@symfony/stimulus-bundle';
-import CheckboxSelectAll from '@stimulus-components/checkbox-select-all'
-
-const app = startStimulusApp();
-
-app.register('checkbox-select-all', CheckboxSelectAll)
--- assets/stimulus_bootstrap.js
+++ assets/stimulus_bootstrap.js
@@ -1,2 +1,9 @@
+import { startStimulusApp } from '@symfony/stimulus-bundle';
+import CheckboxSelectAll from '@stimulus-components/checkbox-select-all'
+
+const app = startStimulusApp();
+
+app.register('checkbox-select-all', CheckboxSelectAll)
+
 // register any custom, 3rd party controllers here
 // app.register('some_controller_name', SomeImportedController);
