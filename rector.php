02b79f2dbf2192cc8ef048aa7dc654a0b596c3bd
elkuku <der.el.kuku@gmail.com>
2026-02-12 08:29:49 -0500

    Configure Symfony container for Rector and update dependencies:
    - Added `tests/symfony-container.php` for Symfony container setup in test environment.
    - Updated `rector.php` to include Symfony services and configure container paths.
    - Bumped various dependencies in `composer.lock` to their latest versions for compatibility.

--- rector.php
+++ rector.php
@@ -3,24 +3,47 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\TypeDeclaration\Rector\StmtsAwareInterface\DeclareStrictTypesRector;
+use Rector\Exception\Configuration\InvalidConfigurationException;
+use Rector\Symfony\Bridge\Symfony\Routing\SymfonyRoutesProvider;
+use Rector\Symfony\Contract\Bridge\Symfony\Routing\SymfonyRoutesProviderInterface;
+use Rector\Symfony\Symfony30\Rector\MethodCall\StringFormTypeToClassRector;
 
-
-return RectorConfig::configure()
-    ->withPaths([__DIR__.'/src', __DIR__.'/tests'])
-    ->withSkip([__DIR__.'/src/Service/PhpXlsxGenerator.php',])
-    ->withPreparedSets(
-        deadCode: true,
-        codeQuality: true,
-        codingStyle: true,
-        typeDeclarations: true,
-        privatization: true,
-        rectorPreset: true,
-        phpunitCodeQuality: true,
-        symfonyCodeQuality: true,
-        symfonyConfigs: true,
-    )
-    ->withComposerBased(twig: true, doctrine: true, phpunit: true, symfony: true)
-    ->withImportNames(removeUnusedImports: true)
-
-    ;
+try {
+    return RectorConfig::configure()
+        ->withPaths([
+            __DIR__.'/src',
+            __DIR__.'/tests',
+        ])
+        ->withSkip([__DIR__.'/src/Service/PhpXlsxGenerator.php',])
+        //
+        ->withSymfonyContainerXml(__DIR__.'/var/cache/dev/App_KernelDevDebugContainer.xml')
+        ->withSymfonyContainerPhp(__DIR__.'/tests/symfony-container.php')
+        ->registerService(SymfonyRoutesProvider::class, SymfonyRoutesProviderInterface::class)
+        ->withPreparedSets(
+            deadCode: true,
+            codeQuality: true,
+            codingStyle: true,
+            typeDeclarations: true,
+            privatization: true,
+            rectorPreset: true,
+            phpunitCodeQuality: true,
+            symfonyCodeQuality: true,
+            symfonyConfigs: true,
+        )
+        ->withAttributesSets(
+            symfony: true,
+            doctrine: true
+        )
+        ->withComposerBased(
+            twig: true,
+            doctrine: true,
+            phpunit: true,
+            symfony: true
+        )
+        ->withImportNames(removeUnusedImports: true)
+        ->withRules([
+            StringFormTypeToClassRector::class,
+        ]);
+} catch (InvalidConfigurationException $e) {
+    echo($e->getMessage());
+}
