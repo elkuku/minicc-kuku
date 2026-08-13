4fec2eda591276993ac2a33ee414efd96c364e4f
elkuku <der.el.kuku@gmail.com>
2026-08-13 09:53:21 +0200

    Update assets

--- importmap.php
+++ importmap.php
@@ -10,231 +10,94 @@
  *     be used as an "entrypoint" (and passed to the importmap() Twig function).
  *
  * The "importmap:require" command can be used to add new entries to this file.
+ *
+ * @return array<string, array{    // Import name as key, description of the imported file as value
+ *     path: string,               // Logical, relative or absolute path to the file
+ *     type?: 'js'|'css'|'json',   // Type of the file, defaults to 'js'
+ *     entrypoint?: bool,          // Whether the file is an entrypoint, for 'js' only
+ * }|array{
+ *     version: string,            // Version of the remote package
+ *     package_specifier?: string, // Remote "package-name/path" specifier, defaults to the import name
+ *     type?: 'js'|'css'|'json',
+ *     entrypoint?: bool,
+ * }>
  */
 return [
-    'app' => [
-        'path' => './assets/app.js',
-        'entrypoint' => true,
-    ],
-    '@symfony/stimulus-bundle' => [
-        'path' => './vendor/symfony/stimulus-bundle/assets/dist/loader.js',
-    ],
-    '@hotwired/stimulus' => [
-        'version' => '3.2.2',
-    ],
-    'chart.js' => [
-        'version' => '4.5.1',
-    ],
-    'bootstrap' => [
-        'version' => '5.3.8',
-    ],
-    '@popperjs/core' => [
-        'version' => '2.11.8',
-    ],
-    'bootstrap/dist/css/bootstrap.min.css' => [
-        'version' => '5.3.8',
-        'type' => 'css',
-    ],
-    'open-iconic/font/css/open-iconic-bootstrap.css' => [
-        'version' => '1.1.1',
-        'type' => 'css',
-    ],
-    'stimulus-use' => [
-        'version' => '0.52.3',
-    ],
-    '@tiptap/core' => [
-        'version' => '3.22.4',
-    ],
-    '@tiptap/pm/state' => [
-        'version' => '3.22.4',
-    ],
-    '@tiptap/pm/view' => [
-        'version' => '3.22.4',
-    ],
-    '@tiptap/pm/keymap' => [
-        'version' => '3.22.4',
-    ],
-    '@tiptap/pm/model' => [
-        'version' => '3.22.4',
-    ],
-    '@tiptap/pm/transform' => [
-        'version' => '3.22.4',
-    ],
-    '@tiptap/pm/commands' => [
-        'version' => '3.22.4',
-    ],
-    '@tiptap/pm/schema-list' => [
-        'version' => '3.22.4',
-    ],
-    'prosemirror-state' => [
-        'version' => '1.4.4',
-    ],
-    'prosemirror-view' => [
-        'version' => '1.41.8',
-    ],
-    'prosemirror-keymap' => [
-        'version' => '1.2.3',
-    ],
-    'prosemirror-model' => [
-        'version' => '1.25.4',
-    ],
-    'prosemirror-transform' => [
-        'version' => '1.12.0',
-    ],
-    'prosemirror-commands' => [
-        'version' => '1.7.1',
-    ],
-    'prosemirror-schema-list' => [
-        'version' => '1.5.1',
-    ],
-    'w3c-keyname' => [
-        'version' => '2.2.8',
-    ],
-    'orderedmap' => [
-        'version' => '2.1.1',
-    ],
-    'prosemirror-view/style/prosemirror.min.css' => [
-        'version' => '1.41.8',
-        'type' => 'css',
-    ],
-    '@tiptap/starter-kit' => [
-        'version' => '3.22.4',
-    ],
-    '@tiptap/extension-blockquote' => [
-        'version' => '3.22.4',
-    ],
-    '@tiptap/extension-bold' => [
-        'version' => '3.22.4',
-    ],
-    '@tiptap/extension-bullet-list' => [
-        'version' => '3.22.4',
-    ],
-    '@tiptap/extension-code' => [
-        'version' => '3.22.4',
-    ],
-    '@tiptap/extension-code-block' => [
-        'version' => '3.22.4',
-    ],
-    '@tiptap/extension-document' => [
-        'version' => '3.22.4',
-    ],
-    '@tiptap/extension-dropcursor' => [
-        'version' => '3.22.4',
-    ],
-    '@tiptap/extension-gapcursor' => [
-        'version' => '3.22.4',
-    ],
-    '@tiptap/extension-hard-break' => [
-        'version' => '3.22.4',
-    ],
-    '@tiptap/extension-heading' => [
-        'version' => '3.22.4',
-    ],
-    '@tiptap/extension-history' => [
-        'version' => '3.22.4',
-    ],
-    '@tiptap/extension-horizontal-rule' => [
-        'version' => '3.22.4',
-    ],
-    '@tiptap/extension-italic' => [
-        'version' => '3.22.4',
-    ],
-    '@tiptap/extension-list-item' => [
-        'version' => '3.22.4',
-    ],
-    '@tiptap/extension-ordered-list' => [
-        'version' => '3.22.4',
-    ],
-    '@tiptap/extension-paragraph' => [
-        'version' => '3.22.4',
-    ],
-    '@tiptap/extension-strike' => [
-        'version' => '3.22.4',
-    ],
-    '@tiptap/extension-text' => [
-        'version' => '3.22.4',
-    ],
-    '@tiptap/pm/dropcursor' => [
-        'version' => '3.22.4',
-    ],
-    '@tiptap/pm/gapcursor' => [
-        'version' => '3.22.4',
-    ],
-    '@tiptap/pm/history' => [
-        'version' => '3.22.4',
-    ],
-    'prosemirror-dropcursor' => [
-        'version' => '1.8.2',
-    ],
-    'prosemirror-gapcursor' => [
-        'version' => '1.4.1',
-    ],
-    'prosemirror-history' => [
-        'version' => '1.5.0',
-    ],
-    'rope-sequence' => [
-        'version' => '1.3.4',
-    ],
-    'prosemirror-gapcursor/style/gapcursor.min.css' => [
-        'version' => '1.4.1',
-        'type' => 'css',
-    ],
-    '@tiptap/extension-table' => [
-        'version' => '3.22.4',
-    ],
-    '@tiptap/extension-table-row' => [
-        'version' => '3.22.4',
-    ],
-    '@tiptap/extension-table-header' => [
-        'version' => '3.22.4',
-    ],
-    '@tiptap/extension-table-cell' => [
-        'version' => '3.22.4',
-    ],
-    '@tiptap/pm/tables' => [
-        'version' => '3.22.4',
-    ],
-    'prosemirror-tables' => [
-        'version' => '1.8.5',
-    ],
-    'prosemirror-tables/style/tables.min.css' => [
-        'version' => '1.8.5',
-        'type' => 'css',
-    ],
-    '@tiptap/extension-text-align' => [
-        'version' => '3.22.4',
-    ],
-    '@tiptap/extension-mention' => [
-        'version' => '3.22.4',
-    ],
-    '@tiptap/suggestion' => [
-        'version' => '3.22.4',
-    ],
-    'tippy.js' => [
-        'version' => '6.3.7',
-    ],
-    '@stimulus-components/checkbox-select-all' => [
-        'version' => '6.1.0',
-    ],
-    '@kurkle/color' => [
-        'version' => '0.4.0',
-    ],
-    '@tiptap/extension-link' => [
-        'version' => '3.22.4',
-    ],
-    '@tiptap/extension-list' => [
-        'version' => '3.22.4',
-    ],
-    '@tiptap/extension-underline' => [
-        'version' => '3.22.4',
-    ],
-    '@tiptap/extensions' => [
-        'version' => '3.22.4',
-    ],
-    '@tiptap/core/jsx-runtime' => [
-        'version' => '3.22.4',
-    ],
-    'linkifyjs' => [
-        'version' => '4.3.2',
-    ],
+    'app' => ['path' => './assets/app.js', 'entrypoint' => true],
+    '@symfony/stimulus-bundle' => ['path' => './vendor/symfony/stimulus-bundle/assets/dist/loader.js'],
+    '@hotwired/stimulus' => ['version' => '3.2.2'],
+    'chart.js' => ['version' => '4.5.1'],
+    'bootstrap' => ['version' => '5.3.8'],
+    '@popperjs/core' => ['version' => '2.11.8'],
+    'bootstrap/dist/css/bootstrap.min.css' => ['version' => '5.3.8', 'type' => 'css'],
+    'open-iconic/font/css/open-iconic-bootstrap.css' => ['version' => '1.1.1', 'type' => 'css'],
+    'stimulus-use' => ['version' => '0.53.0'],
+    '@tiptap/core' => ['version' => '3.30.0'],
+    '@tiptap/pm/state' => ['version' => '3.30.0'],
+    '@tiptap/pm/view' => ['version' => '3.30.0'],
+    '@tiptap/pm/keymap' => ['version' => '3.30.0'],
+    '@tiptap/pm/model' => ['version' => '3.30.0'],
+    '@tiptap/pm/transform' => ['version' => '3.30.0'],
+    '@tiptap/pm/commands' => ['version' => '3.30.0'],
+    '@tiptap/pm/schema-list' => ['version' => '3.30.0'],
+    'prosemirror-state' => ['version' => '1.4.4'],
+    'prosemirror-view' => ['version' => '1.42.2'],
+    'prosemirror-keymap' => ['version' => '1.2.3'],
+    'prosemirror-model' => ['version' => '1.25.11'],
+    'prosemirror-transform' => ['version' => '1.12.0'],
+    'prosemirror-commands' => ['version' => '1.7.2'],
+    'prosemirror-schema-list' => ['version' => '1.5.1'],
+    'w3c-keyname' => ['version' => '2.2.8'],
+    'orderedmap' => ['version' => '2.1.1'],
+    'prosemirror-view/style/prosemirror.min.css' => ['version' => '1.42.2', 'type' => 'css'],
+    '@tiptap/starter-kit' => ['version' => '3.30.0'],
+    '@tiptap/extension-blockquote' => ['version' => '3.30.0'],
+    '@tiptap/extension-bold' => ['version' => '3.30.0'],
+    '@tiptap/extension-bullet-list' => ['version' => '3.30.0'],
+    '@tiptap/extension-code' => ['version' => '3.30.0'],
+    '@tiptap/extension-code-block' => ['version' => '3.30.0'],
+    '@tiptap/extension-document' => ['version' => '3.30.0'],
+    '@tiptap/extension-dropcursor' => ['version' => '3.30.0'],
+    '@tiptap/extension-gapcursor' => ['version' => '3.30.0'],
+    '@tiptap/extension-hard-break' => ['version' => '3.30.0'],
+    '@tiptap/extension-heading' => ['version' => '3.30.0'],
+    '@tiptap/extension-history' => ['version' => '3.30.0'],
+    '@tiptap/extension-horizontal-rule' => ['version' => '3.30.0'],
+    '@tiptap/extension-italic' => ['version' => '3.30.0'],
+    '@tiptap/extension-list-item' => ['version' => '3.30.0'],
+    '@tiptap/extension-ordered-list' => ['version' => '3.30.0'],
+    '@tiptap/extension-paragraph' => ['version' => '3.30.0'],
+    '@tiptap/extension-strike' => ['version' => '3.30.0'],
+    '@tiptap/extension-text' => ['version' => '3.30.0'],
+    '@tiptap/pm/dropcursor' => ['version' => '3.30.0'],
+    '@tiptap/pm/gapcursor' => ['version' => '3.30.0'],
+    '@tiptap/pm/history' => ['version' => '3.30.0'],
+    'prosemirror-dropcursor' => ['version' => '1.8.3'],
+    'prosemirror-gapcursor' => ['version' => '1.4.1'],
+    'prosemirror-history' => ['version' => '1.5.0'],
+    'rope-sequence' => ['version' => '1.3.4'],
+    'prosemirror-gapcursor/style/gapcursor.min.css' => ['version' => '1.4.1', 'type' => 'css'],
+    '@tiptap/extension-table' => ['version' => '3.30.0'],
+    '@tiptap/extension-table-row' => ['version' => '3.30.0'],
+    '@tiptap/extension-table-header' => ['version' => '3.30.0'],
+    '@tiptap/extension-table-cell' => ['version' => '3.30.0'],
+    '@tiptap/pm/tables' => ['version' => '3.30.0'],
+    'prosemirror-tables' => ['version' => '1.8.5'],
+    'prosemirror-tables/style/tables.min.css' => ['version' => '1.8.5', 'type' => 'css'],
+    '@tiptap/extension-text-align' => ['version' => '3.30.0'],
+    '@tiptap/extension-mention' => ['version' => '3.30.0'],
+    '@tiptap/suggestion' => ['version' => '3.30.0'],
+    'tippy.js' => ['version' => '6.3.7'],
+    '@stimulus-components/checkbox-select-all' => ['version' => '6.1.0'],
+    '@kurkle/color' => ['version' => '0.4.0'],
+    '@tiptap/extension-link' => ['version' => '3.30.0'],
+    '@tiptap/extension-list' => ['version' => '3.30.0'],
+    '@tiptap/extension-underline' => ['version' => '3.30.0'],
+    '@tiptap/extensions' => ['version' => '3.30.0'],
+    '@tiptap/core/jsx-runtime' => ['version' => '3.30.0'],
+    'linkifyjs' => ['version' => '4.3.3'],
+    '@floating-ui/dom' => ['version' => '1.8.0'],
+    '@floating-ui/core' => ['version' => '1.8.0'],
+    '@floating-ui/utils' => ['version' => '0.2.12'],
+    '@floating-ui/utils/dom' => ['version' => '0.2.12'],
 ];
